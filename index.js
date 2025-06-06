e4ea6bc740a9dae50ec3d8485cc54309502a66b6
Andre-Makoto <andremolito4@gmail.com>
2025-06-06 18:47:32 -0300

    Now possible to make more than one post

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import ejs from 'ejs';
 
 const port = 3000;
 const app = express();
+const posts = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,19 +26,24 @@ app.get("/new", (req, res) => {
     res.render("new.ejs")
 });
 
-// app.get("/edit", (req, res) => {
-//     res.render("edit.ejs");
-// })
+app.get("/edit", (req, res) => {
+    res.render("edit.ejs");
+})
 
-// app.get("/show", (req, res) => {
-//     res.render("show.ejs");
-// })
+app.get("/show", (req, res) => {
+    res.render("show.ejs");
+})
 
 app.post("/submit", (req, res) => {
     const title = req.body["title"];
     const text = req.body["text"];
-    res.render("index.ejs", {title: title, text: text,});
+    posts.push({title, text});
+    res.render("index.ejs", {posts});
+    // Acredito que assim que requisitar POST, tenho que criar uma nova file para guardar o novo post com o seu respectivo index(posição na array).
+    // Assim consigo usar method get para res.render esse novo post. 
+    // Com isso podemos visualizar o post, editar o post e deletar o post.
 })
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
