62b2387e1038f8b9a919c7bffa9245b187564946
Andre-Makoto <andremolito4@gmail.com>
2025-06-09 16:04:42 -0300

    add __dirname on index.js to make a pathFile

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
+import fs from 'fs'
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const port = 3000;
 const app = express();
@@ -39,6 +43,12 @@ app.post("/submit", (req, res) => {
     const text = req.body["text"];
     posts.push({title, text});
     res.render("index.ejs", {posts});
+
+    const postData = {
+        title: title,
+        text: text,
+        createdAt: new Date()
+    }
     // Acredito que assim que requisitar POST, tenho que criar uma nova file para guardar o novo post com o seu respectivo index(posição na array).
     // Assim consigo usar method get para res.render esse novo post. 
     // Com isso podemos visualizar posts, editar posts e deletar posts.
